6f3a851331f4fa589ecaa4c7139d3caf1f11587e
Triin Vaht <triin.vaht@khk.ee>
2018-09-13 11:02:27 +0300

    Configure a SQLite database connection, create a bookings table

--- web/index.php
+++ web/index.php
@@ -9,6 +9,15 @@ $app['debug'] = true;
 $app->register(new Silex\Provider\TwigServiceProvider(), [
     'twig.path' => __DIR__.'/../views',
 ]);
+
+// Database configuration
+$app->register(new Silex\Provider\DoctrineServiceProvider(), [
+    'db.options' => [
+        'driver' => 'pdo_sqlite',
+        'path' => __DIR__.'/../database/app.db',
+    ],
+]);
+
 $app->get('/bookings/create', function () use ($app) {
     return $app['twig']->render('base.html.twig');
 });
