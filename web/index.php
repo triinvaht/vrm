946edf3b2622d4e089b7cad081d7f427c50ba2e0
Triin Vaht <triin.vaht@khk.ee>
2018-09-13 08:59:13 +0300

    Create front-controller index.php and boostrap Silex

---
+++ web/index.php
@@ -0,0 +1,16 @@
+<?php 
+
+// web/index.php
+require_once __DIR__.'/../vendor/autoload.php';
+
+$app = new Silex\Application();
+
+$app['debug'] = true;
+
+$app->get('/hello', function () {
+    return 'Hello world!';
+});
+
+
+$app->run();
+?>
